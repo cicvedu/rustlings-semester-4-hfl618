07065b30e87a09518e5991bb6f905b21d6e3b377
hfl618 <13102109882@163.com>
2024-07-05 23:09:02 +0800

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -3,10 +3,12 @@
 // Make the code print a greeting to the world.
 //
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
-// hint.
+//hint.
 
-// I AM NOT DONE
+ //I AM NOT DONE
 
 fn main() {
-    println!("Hello {}!");
+    let name = "world";
+    println!("Hello {name}!");
+    
 }
